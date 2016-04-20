e722e13be395de3175e48d0bbc3250fb967395b0
Jeremy <JeremyBernien@GMAIL.COM>
2016-04-19 20:30:16 -0500

    Moved file operations to driver for easiness

--- Main/Driver.py
+++ Main/Driver.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-
+import os.path
 
 class Driver(threading.Thread):
 
@@ -11,33 +11,67 @@ class Driver(threading.Thread):
         self.socket = socket
         print("(NEW THREAD) for IP: " + ip + ", Port = " + port)
 
-    def run(self, socket):
-        print("")
+
+def run(self, socket):
+    self.socket.send("You are connected!".encode())
 
 
+def main(self):
+    serverport = 12009
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 10)
+    serverSocket.bind(('', serverport))
+
+    threads = []
+    # Wait for incoming connections
+    while 1:
+        serverSocket.listen(10)
+        print("(DRIVER) Waiting for connection...")
+        (newsocket, (ip, port)) = serverSocket.accept()
+
+        # Create new thread for new connection
+        thread = Driver(ip, port, newsocket)
+        thread.start()
+
+        # Save started thread into array of threads
+        threads.append(thread)
 
 
+def savemessage(fromUserName, toUserName, message):
+    filename = toUserName + ".txt"
+    open(filename).write(toUserName + ":" + fromUserName + ":" + message)
 
-    def main(self):
-        serverport = 12009
-        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 10)
-        serverSocket.bind(('', serverport))
+def findmessage(username):
+    filename = username + ".txt"
+    for fileline in open(filename + ".txt", "r"):
+        linesplit = fileline.split(":")
+        # Check for a username match
+        if username == linesplit[0]:
+            return username, linesplit[1], linesplit[2]
 
-        threads = []
-        # Wait for incoming connections
-        while 1:
-            serverSocket.listen(10)
-            print("(DRIVER) Waiting for connection...")
-            (newsocket, (ip, port)) = serverSocket.accept()
 
-            # Create new thread for new connection
-            thread = Driver(ip, port, newsocket)
-            thread.start()
+def saveregistration(list):
+    username = list[0]
 
-            # Save started thread into array of threads
-            threads.append(thread)
+    if userisregistered(username):
+        return False
 
+    password = list[1]
+    firstname = list[2]
+    lastname = list[3]
+    email = list[4]
+
+    # Create new file for user
+    filename = username + ".txt"
+    file = open(filename, 'w')
+
+    #Save registration information
+    file.write( username + ":" + password + ":" + firstname + ":" + lastname + ":" + email)
+    return True
+
+def userisregistered(username):
+    filename = username + ".txt"
+    return os.path.isfile(filename)
 
 
 
--- Main/FileOperation.py
+++ Main/FileOperation.py
@@ -1,16 +1,26 @@
+import os.path
 
 def saveMessage(fromUserName, toUserName, message):
-    open("userMessages.txt", 'w').write(fromUserName + ":" + toUserName + ":" + message)
+    filename = toUserName + ".txt"
+    open(filename).write(toUserName + ":" + fromUserName + ":" + message)
 
 
 def findMessage(username):
-    for fileline in open("userMessages.txt", "r"):
+    filename = username + ".txt"
+    for fileline in open(filename + ".txt", "r"):
         linesplit = fileline.split(":")
 
         # Check for a username match
         if username == linesplit[0]:
-            return linesplit[1], linesplit[2]
+            return username, linesplit[1], linesplit[2]
 
 def saveRegistration(username, password):
     open("userRegistrations.txt", "w").write(username + ":" + password )
 
+def userIsRegistered(username, password):
+    filename = username + password + ".txt"
+    os.path.isfile(filename)
+
+def newUserfile(username):
+    filename = username + ".txt"
+    open(filename, 'w')
