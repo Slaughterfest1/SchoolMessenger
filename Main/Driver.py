ab21acd29aab3a335620fee43fe90d2da8f8eb1e
Jeremy <JeremyBernien@GMAIL.COM>
2016-04-19 19:45:41 -0500

    Added multithreading upon socket connection

--- Main/Driver.py
+++ Main/Driver.py
@@ -1,25 +1,45 @@
 import socket
-
-class Driver:
-
-    # Create a welcome socket bound at serverPort
-    serverPort = 12009
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 10)
-    serverSocket.bind(('', serverPort))
-    serverSocket.listen(10)
-
-#Wait for incoming connection
-while 1:
-    #accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-
-   #Start thread upon accepting a connection
-    ct = client_thread(clientsocket)
-    ct.run()
+import threading
 
 
-#Save started thread into array of threads
+class Driver(threading.Thread):
+
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        print("(NEW THREAD) for IP: " + ip + ", Port = " + port)
+
+    def run(self, socket):
+        print("")
+
+
+
+
+
+    def main(self):
+        serverport = 12009
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 10)
+        serverSocket.bind(('', serverport))
+
+        threads = []
+        # Wait for incoming connections
+        while 1:
+            serverSocket.listen(10)
+            print("(DRIVER) Waiting for connection...")
+            (newsocket, (ip, port)) = serverSocket.accept()
+
+            # Create new thread for new connection
+            thread = Driver(ip, port, newsocket)
+            thread.start()
+
+            # Save started thread into array of threads
+            threads.append(thread)
+
+
+
 
 #Pass the connection socket to the new thread
 
